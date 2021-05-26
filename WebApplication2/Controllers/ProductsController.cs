7f55d3399389707e7177a117221404d1a6561182
hassanalgadah-tuwaiq <Hassan.algadah@tuwaiq.edu.sa>
2021-05-26 15:21:42 +0300

    full

---
+++ WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product() {id = 1, name = "item 1 ", price = 99f},
+            new Product() {id = 2, name = "item 2", price = 29f},
+            new Product() {id = 3, name = "item 3", price = 3399f}
+        };
+        // GET
+        public IActionResult Index()
+        {
+            ViewData["products"] = products;
+            return View();
+        }
+        public IActionResult Ramdom()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            Product p;
+            if (id != 0)
+            {
+                ViewData["found"] = false;
+                foreach (var product in products)
+                {
+                    if (id == product.id)
+                    {
+                        ViewData["product"] = product;
+                        ViewData["found"] = true;
+                    }
+                }
+                return View();
+            }
+            ViewData["found"] = true;
+
+            p = new Product() {id = 1, name = "item 1 ", price = 99f};
+            ViewData["product"] = p;
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float price { get; set; }
+    }
+}
